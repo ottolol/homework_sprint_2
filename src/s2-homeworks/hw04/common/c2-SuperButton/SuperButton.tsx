5bf4a64ade4749289a03c6782b77df43b6037937
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-03-18 14:58:45 +0000

    сделал finalClassName

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,12 +17,19 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button + (disabled ? s.disabled : "") + (xType === 'red' ? s.red : "")
+    const finalClassName = `${s.button} 
+        ${disabled ? s.disabled : ''} 
+        ${xType === 'red' ? s.red : ''} 
+        ${xType === 'secondary' ? s.secondary : ''} 
+        ${className ? ' ' + className : ''}`;
+
+
+    //const finalClassName = s.button 
         // + (disabled
         //         ? ...
         //         : xType === 'red'
         //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+        //+ (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
         <button
