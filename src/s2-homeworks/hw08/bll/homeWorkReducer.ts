65313bf9236082eb3c1a068d55287ca0b0703f54
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-04-18 13:08:36 +0000

    Сделал ДЗ-4 (HW08), Спринт 3. Доделять стили и тесты

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const sortedState = [...state];
+            return sortedState.sort((a, b) => {
+                if (action.payload === 'up') {
+                    return a.name > b.name ? 1 : -1
+                } else {
+                    return a.name < b.name ? 1 : -1
+                }
+            }) // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return action.payload === 18 ? state.filter(s => s.age > 18) : state;
+            // need to fix
         }
         default:
             return state
