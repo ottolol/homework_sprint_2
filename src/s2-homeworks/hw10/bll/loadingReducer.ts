f669811a3dd262f2ef794525efbc15c4cb910cf1
ottolol <alexsveshnik@gmail.com>
2025-05-02 16:02:11 +0300

    Сделал ДЗ-2 (HW10), Спринт 4.

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,35 @@
-const initState = {
-    isLoading: false,
-}
+const initState: initStateType = {
+  isLoading: false,
+};
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
+type initStateType = {
+  isLoading: boolean;
+};
 
-        default:
-            return state
-    }
-}
+export const loadingReducer = (
+  state: initStateType = initState,
+  action: LoadingActionType
+): initStateType => {
+  // fix any
+  switch (action.type) {
+    // пишет студент  // need to fix
+    case "CHANGE_LOADING":
+      return {
+        ...state,
+        isLoading: action.isLoading,
+      };
+
+    default:
+      return state;
+  }
+};
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+  type: "CHANGE_LOADING";
+  isLoading: boolean;
+};
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
-})
+  type: "CHANGE_LOADING",
+  isLoading,
+});
