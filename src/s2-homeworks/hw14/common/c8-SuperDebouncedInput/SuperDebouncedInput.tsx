e4cd72bc116260ed0d000cf79a49c222a7bfcad8
ottolol <alexsveshnik@gmail.com>
2025-06-02 18:04:26 +0300

    Сделал ДЗ-2 (HW14), Спринт 5.

--- src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
+++ src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -1,49 +1,80 @@
-import React, {DetailedHTMLProps, InputHTMLAttributes, ReactNode, useState} from 'react'
-import SuperInputText from '../../../hw04/common/c1-SuperInputText/SuperInputText'
+import React, {
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import SuperInputText from "../../../hw04/common/c1-SuperInputText/SuperInputText";
 
 // тип пропсов обычного инпута
-type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement>
+type DefaultInputPropsType = DetailedHTMLProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
 
 // здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута, кроме type
 // (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
-export type SuperDebouncedInputPropsType = Omit<DefaultInputPropsType, 'type'> & {
-    // и + ещё пропсы которых нет в стандартном инпуте
-    onChangeText?: (value: string) => void
-    onEnter?: () => void
-    error?: ReactNode
-    spanClassName?: string
-} // илм экспортировать тип SuperInputTextPropsType
-    & { // плюс специальный пропс SuperPagination
-    onDebouncedChange?: (value: string) => void
-}
+export type SuperDebouncedInputPropsType = Omit<
+  DefaultInputPropsType,
+  "type"
+> & {
+  // и + ещё пропсы которых нет в стандартном инпуте
+  onChangeText?: (value: string) => void;
+  onEnter?: () => void;
+  error?: ReactNode;
+  spanClassName?: string;
+} & {
+  // илм экспортировать тип SuperInputTextPropsType
+  // плюс специальный пропс SuperPagination
+  onDebouncedChange?: (value: string) => void;
+};
 
-const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
-    {
-        onChangeText,
-        onDebouncedChange,
+const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = ({
+  onChangeText,
+  onDebouncedChange,
 
-        ...restProps // все остальные пропсы попадут в объект restProps
-    }
-) => {
-    const [timerId, setTimerId] = useState<number | undefined>(undefined)
+  ...restProps // все остальные пропсы попадут в объект restProps
+}) => {
+  const [timerId, setTimerId] = useState<number | undefined>(undefined);
+  const valueRef = useRef<string>("");
 
-    const onChangeTextCallback = (value: string) => {
-        onChangeText?.(value)
+  const lastValue = useRef<string>("");
 
-        if (onDebouncedChange) {
-            // делает студент
+  const onChangeTextCallback = useCallback(
+    (value: string) => {
+      console.log("onChangeTextCallback", value);
+      valueRef.current = value;
 
-            // остановить предыдущий таймер
-            // запустить новый на 1500ms, в котором вызовется функция
+      // Не триггерим debounce, если значение не изменилось
+      if (value === lastValue.current) return;
+      lastValue.current = value;
 
-            //
-        }
-    }
+      onChangeText?.(value);
 
-    return (
-        <SuperInputText onChangeText={onChangeTextCallback} {...restProps}/>
-    )
-}
+      if (onDebouncedChange) {
+        if (timerId) clearTimeout(timerId);
 
-export default SuperDebouncedInput
+        const id = setTimeout(() => {
+          onDebouncedChange(valueRef.current);
+        }, 1500);
+
+        setTimerId(id as any);
+      }
+    },
+    [onChangeText, onDebouncedChange, timerId]
+  );
+
+  // Очистка таймера при размонтировании компонента
+  useEffect(() => {
+    return () => {
+      if (timerId) window.clearTimeout(timerId);
+    };
+  }, [timerId]);
+
+  return <SuperInputText onChangeText={onChangeTextCallback} {...restProps} />;
+};
+
+export default SuperDebouncedInput;
